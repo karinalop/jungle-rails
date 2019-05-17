03eb77673dfd499d606ff071e83b5cbd6eba9d41
karinalop <karina.popeye@gmail.com>
2019-05-17 23:23:05 +0000

    Inserts review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,18 @@ class ReviewsController < ApplicationController
     @reviews =@product.reviews
   end
 
+  def create
+    puts params
+    #raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(allowed_params)
+    @review.user = current_user
+    @review.save!
+    redirect_to product_path(@product)
+  end
+
+  private
+    def allowed_params
+      params.require(:review).permit(:rating, :description)
+    end
 end
