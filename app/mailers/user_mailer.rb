2902d9002e46d1d3466ef07de969bbf9bbcb4367
karinalop <karina.popeye@gmail.com>
2019-05-17 18:35:14 +0000

    Remove unnecessary variables

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
 
   def order_receipt(order)
-    @user = order.email
-    @id = order.id
     @order = order
-    mail(to: @user, subject: "Receipt of your order #{@order.id}")
+    mail(to: @order.email, subject: "Receipt of your order #{@order.id}")
   end
 end
